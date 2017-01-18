71d4d64ef9f1d0227eeeb6db5434fce800741ea1
EgerbSher <egorka_k2009@ukr.net>
2017-01-18 12:19:26 +0200

    Ver 1.0

---
+++ VkGraffiti/VkGraffiti/GlobalVars.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VkGraffiti
+{
+    class GlobalVars
+    {
+        public static string token;
+        public static string user_id;
+    }
+}
